5930bc6c29a71b384c6db7434b9b36ac957d6be1
Ivan <anelkachu@gmail.com>
2017-10-07 09:23:54 +0200

    Test endpoint

---
+++ src/main/java/itemorigin/controller/TestController.java
@@ -0,0 +1,30 @@
+package itemorigin.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value = "/testCompleto", produces = "application/json")
+	public Map<String, String> test() throws IOException {
+		Map<String, String> mapReturn = new HashMap<String, String>();
+
+		mapReturn.put("partyName", "ALTER FARMACIA, S.A.");
+		mapReturn.put("caName", "Comunidad de Madrid");
+		mapReturn.put("city", "Madrid");
+		mapReturn.put("address", "CL Mateo Inurria, 30");
+		mapReturn.put("countryCode", "ES");
+		mapReturn.put("responseTime", "123");
+		mapReturn.put("postalCode", "28050");
+		mapReturn.put("lastChange", "2017-10-06T01:53:05.422");
+		mapReturn.put("caCode", "13");
+		mapReturn.put("countryName", "España");
+
+		return mapReturn;
+	}
+}
