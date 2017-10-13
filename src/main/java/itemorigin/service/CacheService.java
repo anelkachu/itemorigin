7af212b900a91557a9bd39356e6069d0871001e1
Ivan <anelkachu@gmail.com>
2017-10-13 11:35:32 +0200

    Country code

--- src/main/java/itemorigin/service/CacheService.java
+++ src/main/java/itemorigin/service/CacheService.java
@@ -48,11 +48,11 @@ public class CacheService {
 		} else {
 			Map<String, String> mapReturn = new HashMap<String, String>();
 			String countryCode = countryService.getCountryCodeByGlnId(paddedGtin13);
+			mapReturn.put("countryCode", countryCode);
 			try {
 				if (!Strings.isNullOrEmpty(countryCode) && countryCode.equalsIgnoreCase("ES")) {
 					// Aecoc client
 					mapReturn = NewAecocClient.getAecocInfo(paddedGtin14);
-					mapReturn.put("countryCode", countryCode);
 				} else {
 					// Gepir limited client
 					if (abroadCounter.intValue() < 30) {
